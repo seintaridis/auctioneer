42377c5c0be95f1fd7983450619d3698c89ce6f8
kostas seintarides <dimitris@kostas-macbook-air.home>
2016-07-24 03:16:35 +0200

    test jpa

--- src/main/java/ted/Application.java
+++ src/main/java/ted/Application.java
@@ -1,11 +1,12 @@
 package ted;
 
+
+import javax.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.PathVariable;
-
 
 @Configuration
 @ComponentScan
@@ -13,6 +14,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        //SpringApplication.run(Application.class, args);
+        Users user = new Users();
+        user.setUserID(24);
+        user.setPassword("test")
+                .setUsername("Bangladesh")
+                .setLastName("1vfvf")
+                .setFirstName("Poribagh");
+
+        EntityManager em = PersistenceManager.INSTANCE.getEntityManager();
+        em.getTransaction()
+                .begin();
+        em.persist(user);
+        em.getTransaction()
+                .commit();
+        em.close();
+        PersistenceManager.INSTANCE.close();
     }
 }
---
+++ src/main/java/ted/users.java
@@ -0,0 +1,7 @@
+package ted;
+
+/**
+ * Created by dimitris on 7/23/16.
+ */
+public class users {
+}
