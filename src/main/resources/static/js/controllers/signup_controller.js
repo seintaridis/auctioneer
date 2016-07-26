0bfe5bd43a496389f9b53629dcf01535202c4ed8
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2016-07-26 22:00:49 +0200

    * Adds password validation. * Adds email validation. * Adds missing field validation. * Adds phone length validation. * Adds password length validation. * Closes #6.

--- src/main/resources/static/js/controllers/signup_controller.js
+++ src/main/resources/static/js/controllers/signup_controller.js
@@ -17,8 +17,25 @@ app.controller('SignUpController', ['$scope', '$http', '$location', '$cookieStor
         address: ''
     };
 
+
+    $scope.vaildateEmail = function(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    };
+
+
+
     $scope.signup = function() {
         $scope.authenticated = true;
+
+        var error = false;
+        var miss_error = false;
+
+        $scope.phone_number_error = false;
+        $scope.pass_error = false;
+        $scope.pass_len_error = false;
+        $scope.mail_error= false;
+
 
         var request = {
             username: $scope.credentials.username,
@@ -33,6 +50,76 @@ app.controller('SignUpController', ['$scope', '$http', '$location', '$cookieStor
             afm: $scope.credentials.afm,
             address: $scope.credentials.address
         };
+
+        // UNCOMMENT THE FOLLOWING TO ENABLE MISSING FIELDS VALIDATION
+        // if ($scope.credentials.username === '') {
+        //     $scope.username_missing_error = 'set';
+        //     miss_error = true;
+        // }
+        // if ($scope.credentials.first_name === '') {
+        //     $scope.first_name_missing_error = 'set';
+        //     miss_error = true;
+        // }
+        // if ($scope.credentials.last_name === '') {
+        //     $scope.last_name_missing_error = 'set';
+        //     miss_error = true;
+        // }
+        // if ($scope.credentials.password === '') {
+        //     $scope.password_missing_error = 'set';
+        //     miss_error = true;
+        // }
+        // if ($scope.credentials.rep_password === '') {
+        //     $scope.rep_password_missing_error = 'set';
+        //     miss_error = true;
+        // }
+        // if ($scope.credentials.mail === '') {
+        //     $scope.mail_missing_error = 'set';
+        //     miss_error = true;
+        // }
+        // if ($scope.credentials.phone_number === '') {
+        //     $scope.phone_number_missing_error = 'set';
+        //     miss_error = true;
+        // }
+        // if ($scope.credentials.latitude === '') {
+        //     $scope.latitude_missing_error = 'set';
+        //     miss_error = true;
+        // }
+        // if ($scope.credentials.longtitude === '') {
+        //     $scope.longtitude_missing_error = 'set';
+        //     miss_error = true;
+        // }
+        // if ($scope.credentials.afm === '') {
+        //     $scope.afm_missing_error = 'set';
+        //     miss_error = true;
+        // }
+        // if ($scope.credentials.address === '') {
+        //     $scope.address_missing_error = 'set';
+        //     miss_error = true;
+        // }
+
+
+        if ($scope.credentials.phone_number.length < 10 && miss_error == false) {
+            $scope.phone_number_error = 'set';
+            error = true;
+        }
+
+        if ($scope.credentials.password.length < 5 && miss_error == false) { // TODO: Should prevent pass_error to be sent.p
+            $scope.pass_len_error = 'set';
+            error = true;
+        }
+
+        if ($scope.credentials.password !== $scope.credentials.rep_password && miss_error == false) {
+            $scope.pass_error = 'set';
+            error = true;
+        }
+
+        if ($scope.vaildateEmail($scope.credentials.mail) != true && miss_error == false) {
+            $scope.mail_error = 'set';
+            error=true;
+        }
+
+
+        if (error ||  miss_error) return;
 
         console.log(request);
 
