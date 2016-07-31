4a860cf49f8f355dff4c32081b256ab87c315093
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2016-07-31 15:01:58 +0200

    * Adds `user not found` error on frontend.

--- src/main/resources/static/js/controllers/login_controller.js
+++ src/main/resources/static/js/controllers/login_controller.js
@@ -16,11 +16,18 @@ app.controller('UserController', ['$scope', '$location', 'RequestServices', 'sha
         };
 
         RequestServices.login(request).then(function (response){
-           if (response.role === 'admin') { $location.path("/admin_panel"); }
-           else { $location.path("/"); }
+            console.log(response);
+            if (response.status == 400) {
+                $scope.user_error = 'set'
+            }
+            else {
+                if (response.role === 'admin') { $location.path("/admin_panel"); }
+                else { $location.path("/"); }
 
-           sharedProperties.setSuccSignup(response.verified);
-            console.log(response.verified);
+                sharedProperties.setSuccSignup(response.verified);
+                console.log(response.verified);
+            }
+
 
         })
 
